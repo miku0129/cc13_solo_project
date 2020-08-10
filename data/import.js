484528829c39ce9765a51bd24960ce4f00f85fc6
MikuSano <svartkatt39@gmail.com>
2020-08-10 20:37:02 +0900

    server working

--- data/import.js
+++ data/import.js
@@ -2,19 +2,28 @@ require("dotenv").config();
 const fs = require("fs");
 const db = require("../server/knex.js");
 
-(async()=>{
-    try{
-        const addreeses = JSON.parse(fs.readFileSync("./data/address.json"));
-        for(const address of addresses){
-            const id//???
+(async () => {
+  try {
+    const addresses = JSON.parse(fs.readFileSync("./data/addresses.json"));
+    for (const address of addresses) {
+      const id = address.id;
+      const name = address.name;
+      const ad = address.ad;
+      const word = address.word;
+      const city = address.city;
+      const postal_code = address.postal_code;
 
-            const result = await db("addresses").insert({
-                id,//???
-            });
-            console.log("id",id);
-            console.log(result);
-        }
-    } catch (err) {
-        console.error("Error inserting records",err);
+      const result = await db("addresses").insert({
+        id,
+        name,
+        ad,
+        word,
+        city,
+        postal_code,
+      });
+      console.log(result);
     }
+  } catch (err) {
+    console.error("Error inserting records", err);
+  }
 })();
--- migrations/20200810190930_bekind.js
+++ migrations/20200810190930_bekind.js
@@ -1,8 +1,17 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("addresses", (table) => {
+    table.increments().index();
+
+    table.float("name");
+
+    table.float("ad");
+
+    table.text("word");
+
+    table.text("city");
+
+    table.text("postal_code");
+  });
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = function(knex, Promise) {};
