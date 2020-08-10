645214e169faa21e4b238aa346fd2c079900fb29
MikuSano <svartkatt39@gmail.com>
2020-08-10 20:47:33 +0900

    fixed migration file

--- migrations/20200810190930_bekind.js
+++ migrations/20200810190930_bekind.js
@@ -2,15 +2,15 @@ exports.up = function(knex) {
   return knex.schema.createTable("addresses", (table) => {
     table.increments().index();
 
-    table.float("name");
+    table.test("name");
 
-    table.float("ad");
+    table.text("ad");
 
     table.text("word");
 
     table.text("city");
 
-    table.text("postal_code");
+    table.float("postal_code");
   });
 };
 
